dac49c0fb1bfbc3b1a9a81842f7cab94d8a2e37e
Jagan Teki <jagan@amarulasolutions.com>
2018-10-06 23:14:06 +0530

    A64 QT build
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- package/sunxi-mali-mainline/sunxi-mali-mainline.mk
+++ package/sunxi-mali-mainline/sunxi-mali-mainline.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-SUNXI_MALI_MAINLINE_VERSION = cb3e8ece9b2c3a70cbeb3204cd6f30eceaa32023
+#SUNXI_MALI_MAINLINE_VERSION = cb3e8ece9b2c3a70cbeb3204cd6f30eceaa32023
+SUNXI_MALI_MAINLINE_VERSION = 418f55585e76f375792dbebb3e97532f0c1c556d
 SUNXI_MALI_MAINLINE_SITE = $(call github,free-electrons,mali-blobs,$(SUNXI_MALI_MAINLINE_VERSION))
 SUNXI_MALI_MAINLINE_INSTALL_STAGING = YES
 SUNXI_MALI_MAINLINE_PROVIDES = libegl libgles
@@ -14,10 +15,8 @@ SUNXI_MALI_MAINLINE_REV = $(call qstrip,$(BR2_PACKAGE_SUNXI_MALI_MAINLINE_REVISI
 define SUNXI_MALI_MAINLINE_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/lib $(STAGING_DIR)/usr/include
 
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/lib/lib_fb_dev/* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/arm64/fbdev/* \
 		$(STAGING_DIR)/usr/lib/
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/include/* \
-		$(STAGING_DIR)/usr/include/
 
 	$(INSTALL) -D -m 0644 package/sunxi-mali-mainline/egl.pc \
 		$(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
@@ -27,7 +26,7 @@ endef
 
 define SUNXI_MALI_MAINLINE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/lib/lib_fb_dev/* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/arm64/fbdev/* \
 		$(TARGET_DIR)/usr/lib/
 endef
 
