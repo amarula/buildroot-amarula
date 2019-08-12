2b64da4ca666f6615ebac0cead366b37a4dffbdf
Jagan Teki <jagan@amarulasolutions.com>
2019-08-12 15:21:15 +0200

    BPI-m2m
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- board/bananapi/bananapi-m2m/boot.cmd
+++ board/bananapi/bananapi-m2m/boot.cmd
@@ -2,6 +2,6 @@ setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait rw
 
 mmc dev 0
 fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun7i-a20-bananapi.dtb
+fatload mmc 0 $fdt_addr_r sun8i-r16-bananapi-m2m.dtb
 
 bootz $kernel_addr_r - $fdt_addr_r
